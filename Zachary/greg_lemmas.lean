9bb4529d6f86a8e3286aadbc1f9000ead6c3b1c3
Greg <gb@example.com>
2023-02-26 22:29:54 -0800

    made a theorem

--- Zachary/greg_lemmas.lean
+++ Zachary/greg_lemmas.lean
@@ -11,6 +11,6 @@ import data.rat.basic
 lemma p_is_positive (q : ℝ) (h_q_gt_one : q > 1) : (q / (q - 1)) > 0 :=
 begin
   have hq : q - 1 > 0 := by linarith,
-  by_contra hc, push_neg at hc,
-  sorry,
+  have hqp: q > 0 := by linarith,
+  exact div_pos hqp hq,
 end
