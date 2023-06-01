7b859cacf1a2539c790d81fdd93a09bd45fc8c73
qrico64 <qirico@outlook.com>
2023-06-01 14:10:24 -0700

    update

--- Edward & Rico/core.lean
+++ Edward & Rico/core.lean
@@ -1,287 +1,203 @@
-import data.real.basic
 import tactic
-import dirangle
+import data.real.basic -- ℝ 
+import data.complex.basic -- ℂ
+import analysis.special_functions.trigonometric.angle -- real.angle
+import analysis.special_functions.complex.arg -- complex.arg
 
-open real.dirangle
+-- noncomputable def dist (a b : ℂ) : ℝ := complex.abs(b-a) -- EY: turns out this is already built-in
+noncomputable def angle (a b c : ℂ) : real.angle := ((c-b)/(a-b)).arg
 
-structure Point : Type
-structure Line : Type
+def circle (center : ℂ) (radius : ℝ) := metric.sphere center radius -- EY: allowing degen circles (negative radius)
+def line (a b : ℂ) := {x : ℂ | ∃ t : ℝ, x = a * t + b * (1-t)} -- EY: allowing degen lines
 
-axiom line_exists : ∃ l : Line, true
 
-constant lies_on_def : Point → Line → Prop
-
-notation (name := what_does_this_mean) p ` lies_on `:50 l:50 := lies_on_def p l
-
-axiom line_by_two_points (p q : Point): p ≠ q → ∃ l : Line, p lies_on l ∧ q lies_on l
-axiom line_by_two_points_unique (p q : Point) (l1 l2 : Line) : p ≠ q → p lies_on l1 → p lies_on l2 → q lies_on l1 → q lies_on l2 → l1 = l2
-axiom exist_points_on_line (l : Line) : ∃ p q : Point, p ≠ q ∧ p lies_on l ∧ q lies_on l
-
--- given two distinct points, returns a line through both of them
-noncomputable def make_line (p q : Point) (h0 : p ≠ q) : Line := (line_by_two_points p q h0).some
-
-theorem intersection_unique (l1 l2 : Line) (p q : Point) : l1 ≠ l2 → p lies_on l1 → p lies_on l2 → q lies_on l1 → q lies_on l2 → p = q :=
+def Lines : set (set ℂ) := {s : set ℂ | ∃ (a b : ℂ), s = line a b } -- set of all lines 
+def Circles : set (set ℂ) := {s : set ℂ | ∃ (c : ℂ) (r : ℝ), s = circle c r } -- set of all circles
+lemma circle_defn (circ) : (circ ∈ Circles) → ∃ (c : ℂ), ∃ (r : ℝ), circ = circle c r  :=
 begin
-  intro lineq,
-  intro ponl1,
-  intro ponl2,
-  intro qonl1,
-  intro qonl2,
-  by_contra pneq,
-  change p ≠ q at pneq,
-  apply lineq,
-  apply line_by_two_points_unique p q l1 l2,
-  exact pneq,
-  exact ponl1,
-  exact ponl2,
-  exact qonl1,
-  exact qonl2,
+  intro h,
+  exact has_mem.mem.out h,
+end
+noncomputable def circle_center (circ : set ℂ) (h : circ ∈ Circles) : ℂ := (circle_defn circ h).some
+-- noncomputable def circle_radius (circ : set ℂ) (h : circ ∈ Circles) : ℝ := 
+
+theorem angle_antisymm (a b c : ℂ) (hab: a ≠ b) (hcb: c ≠ b) : angle a b c + angle c b a = 0 :=
+begin
+  rw angle,
+  rw angle,
+  have hab' : a - b ≠ 0,
+    exact sub_ne_zero.mpr hab,
+  have hcb' : c - b ≠ 0,
+    exact sub_ne_zero.mpr hcb,
+  rw complex.arg_div_coe_angle hab' hcb',
+  rw complex.arg_div_coe_angle hcb' hab',
+  simp,
 end
 
-lemma points_exist : ∃ p q : Point, p ≠ q :=
+theorem angle_linear (p a b c : ℂ) (ha : a≠p) (hb: b≠p) (hc: c≠p) : angle a p b + angle b p c = angle a p c :=
 begin
-  have l := line_exists.some,
-  have h := (exist_points_on_line l),
-  cases h with x hx,
-  cases hx with q hq,
-  use x,
-  use q,
-  cases hq with h1 h2,
-  assumption, -- props to @Edward Yu
+  rw angle,
+  rw angle,
+  rw angle,
+  have ha : a-p ≠ 0, exact sub_ne_zero.mpr ha,
+  have hb : b-p ≠ 0, exact sub_ne_zero.mpr hb,
+  have hc : c-p ≠ 0, exact sub_ne_zero.mpr hc,
+  rw complex.arg_div_coe_angle hb ha,
+  rw complex.arg_div_coe_angle hc hb,
+  rw complex.arg_div_coe_angle hc ha,
+  simp,
 end
 
-lemma exists_line_through_point (p : Point) : ∃ l : Line, p lies_on l :=
+lemma angle_self_zero_reducelem (x : real.angle) : x + x = x → x = 0 :=
+  begin
+    exact add_left_eq_self.mp,
+  end
+theorem angle_self_zero (p a : ℂ) (ha : a≠p) : angle a p a = 0 :=
 begin
-  cases points_exist with q h2,
-  cases h2 with r qneqr,
-  by_cases p = q,
-  {
-    cases line_by_two_points q r qneqr with l hl,
-    use l,
-    rw h,
-    cases hl with hleft hright,
-    exact hleft,
-  },
-  {
-    change p ≠ q at h,
-    cases line_by_two_points p q h with l hl,
-    use l,
-    cases hl with hleft hright,
-    exact hleft,
-  },
+  have test,
+    exact angle_linear p a a a ha ha ha,
+  apply angle_self_zero_reducelem (angle a p a),
+  exact test,
 end
 
--- COLLINEAR
-
-def collinear (p q r : Point) : Prop := ∃ l : Line, p lies_on l ∧ q lies_on l ∧ r lies_on l
--- Note that p q r can be equal.
-
-lemma collinear_self_self (p : Point) : collinear p p p :=
+theorem line_linearcombination (a b : ℂ) (r: ℝ) : (r ≠ 1) → line a b = line a (r*a + (1-r)*b) :=
 begin
-  cases exists_line_through_point p with l hl,
-  use l,
-  rw and_self,
-  rw and_self,
-  exact hl,
-end
-
-lemma collinear_self (p q : Point) : collinear p p q :=
-begin
-  by_cases h0 : p = q,
-  {
-    rw h0,
-    exact collinear_self_self q,
-  },
-  {
-    cases line_by_two_points p q h0 with l lh,
-    use l,
-    rw ← and_assoc,
-    rw and_self,
-    exact lh,
-  },
-end
-
-def intersect_line_line (l1 l2 : Line) : Prop := ∃ p : Point, p lies_on l1 ∧ p lies_on l2
-
--- BETWEEN
-
-constant between: Point → Point → Point → Prop
--- between p q r ↔ q is between p and r (can be equal).
-
-axiom between_implies_collinear (p q r : Point) : between p q r → collinear p q r
-
--- Hilbert II.1, page 4.
-axiom between_commutative (p q r : Point) : between p q r ↔ between r q p
-
--- I think this is necessary..?
-axiom between_not_outside (p q : Point) : p ≠ q → ¬between p q p
-
--- EY: I just want "exactly one of these three things is true" and this feels like an ugly solution
--- Rico: I don't see any better way to do it unfortunately...
-
--- Hilbert II.3 (modified).
-axiom between_unique (p q r : Point) : between p q r → p = q ∨ (¬ between q p r)
-
--- Hilbert II.3 (modified).
-axiom between_exists (p q r : Point) : collinear p q r → between q p r ∨ between p q r ∨ between p r q
-
--- Hilbert II.2 (modified)
-axiom exists_point_between (p q : Point) : p ≠ q → ∃ r : Point, r ≠ p ∧ r ≠ q ∧ between p r q
-
--- Hilbert II.2 (modified)
-axiom exists_point_outside (p q : Point) : ∃ r : Point, r ≠ p ∧ between r p q
-
--- TODO axiom ii-4 in hilbert? what even is this, how do i write it nicely, why does it exist
-
-lemma between_self_self (p : Point) : between p p p :=
-begin
-  have h0 : between p p p ∨ between p p p ∨ between p p p,
-  {
-    apply between_exists p p p,
-    exact collinear_self_self p,
-  },
-  {
-    rw or_self at h0,
-    rw or_self at h0,
-    exact h0,
-  }
-end
-
-lemma between_self (p q : Point) : between p p q :=
-begin
-  have col := collinear_self p q,
-  have h := between_exists p p q col,
-  rw ← or_assoc at h,
-  rw or_self at h,
-  cases h with h0 h0,
-  {
-    exact h0,
-  },
-  {
-    by_cases peq : p = q,
+  intro hr,
+  rw line,
+  rw line,
+  apply set.eq_of_subset_of_subset,
     {
-      rw peq,
-      exact between_self_self q,
+      intros x hx,
+      refine set.mem_def.mpr _,
+      refine set.mem_def.mpr _,
+      cases has_mem.mem.out hx with t ht,
+      use (r-t)/(r-1),
+      rw ht,
+      sorry, -- EY: this is just alg
     },
     {
-      exfalso,
-      apply between_not_outside p q peq,
-      exact h0,
-    },
-  },
+      intros x hx,
+      refine set.mem_def.mpr _,
+      refine set.mem_def.mpr _,
+      cases has_mem.mem.out hx with t ht,
+      use -t*r + t + r,
+      rw ht,
+      sorry,  -- EY: this is just alg
+    }
 end
 
-def same_side (o p q : Point) : Prop := o ≠ p ∧ o ≠ q ∧ (between o p q ∨ between o q p)
+noncomputable def midpoint (a b : ℂ) := (a+b)/2
 
-def opposite_side (o p q : Point) : Prop := o ≠ p ∧ o ≠ q ∧ (between p o q)
-
--- SEGMENT
-
-structure Segment :=
-segment :: (first : Point) (second : Point)
--- first can be equal to second.
-
-open Segment
-
-axiom segment_symm (p q : Point) : segment p q = segment q p
-
-def lies_on_seg_def (p : Point) (s : Segment) : Prop := between s.first p s.second
-
-def intersect_seg_seg (s1 s2 : Segment) : Prop := ∃ p : Point, lies_on_seg_def p s1 ∧ lies_on_seg_def p s2
-
-def intersect_line_seg (l1 : Line) (s1 : Segment) : Prop := ∃ p : Point, p lies_on l1 ∧ lies_on_seg_def p s1
-
--- Hilbert II.5, page 4.
-axiom intersect_part (p q r : Point) (l : Line) : intersect_line_seg l (segment p r) → intersect_line_seg l (segment p q) ∨ intersect_line_seg l (segment q r)
-
--- DISTANCE
-
-axiom distance : Point → Point → ℝ
-
--- Implicitly defines Hilbert IV.2, page 8.
-noncomputable def length (s : Segment) : ℝ := distance s.first s.second
-
-lemma distance_is_length (p q : Point) : distance p q = length (segment p q) :=
+lemma this_is_that1 (v : ℂ) : ‖v‖ = complex.abs (v) :=
 begin
-  rw length,
+  exact v.norm_eq_abs,
 end
 
-lemma distance_commutative (p q : Point) : distance p q = distance q p :=
+lemma this_is_that2 (P Q : ℂ) : dist P Q = ‖P - Q‖ :=
 begin
-  rw distance_is_length,
-  rw distance_is_length,
-  rw segment_symm,
+  exact dist_eq_norm P Q,
 end
 
--- Hilbert IV.3, page 8.
-axiom distance_transitive (p q r : Point) (h0 : between p q r) : distance p q + distance q r = distance p r
-
-lemma distance_reflexive (p : Point) : distance p p = 0 :=
+lemma this_is_that3 (P Q : ℂ) : dist P Q = complex.abs (P - Q) :=
 begin
-  have col := between_self_self p,
-  have trans := distance_transitive p p p col,
-  linarith,
+  exact complex.dist_eq P Q,
 end
 
--- Hilbert IV.1, page 8.
-axiom exists_seg_congruent_same_side (o p : Point) (l : Line) (d : ℝ) : o lies_on l → p lies_on l → o ≠ p → d > 0 → ∃ q : Point, same_side o p q ∧ distance o q = d
-
--- ANGLES
-
--- EY : oh no dirangle needs a has_zero (!!)
-
-constant Angle : Line → Line → real.dirangle
-axiom angle_line_refl (a : Line) : Angle a a = 0
-axiom angle_line_addition (a b c : Line) : Angle a b + Angle b c = Angle a c
-lemma angle_line_antisymm : ∀ a b : Line, Angle a b = - Angle b a :=
+lemma circle_lemma_1 (X : ℂ) (O : ℂ) (r : ℝ) : X ∈ circle O r → ∃ d : ℂ, ‖d‖ = r ∧ X = O + d :=
 begin
-  intros a b,
-  apply add_eq_zero_iff_eq_neg.mp,
-  rw angle_line_addition a b a,
-  rw angle_line_refl,
-end
-
-def parallel (l1 l2 : Line) := Angle l1 l2 = 0
-lemma self_parallel : ∀ a : Line, parallel a a :=
-begin
-  intro a,
-  apply angle_line_refl a,
-end
-
--- In order to prove parallel_postulate_unique.
-axiom def_parallel (l1 l2 : Line) : parallel l1 l2 ↔ l1 = l2 ∨ ¬intersect_line_line l1 l2
-
--- Hilbert III, page 7.
-axiom parallel_postulate (p : Point) (θ : real.dirangle) (l0 : Line) : ∃ l : Line, p lies_on l ∧ Angle l l0 = θ
-lemma parallel_postulate_unique (p : Point) (l0 l1 : Line) : p lies_on l0 → p lies_on l1 → parallel l0 l1 → l0 = l1 :=
-begin
-  intros ponl0 ponl1 prll,
-  rw def_parallel at prll,
-  cases prll,
+  rw circle,
+  intro h,
+  rw mem_sphere_iff_norm at h,
+  use X - O,
+  split,
   {
-    exact prll,
+    exact h,
   },
   {
-    exfalso,
-    apply prll,
-    use p,
-    split,
-    {
-      exact ponl0,
-    },
-    {
-      exact ponl1,
-    },
+    norm_num,
   },
 end
 
-
--- EY : this works, but I'd rather use the (unfinished) commented thing below because you'll see it's more convenient
--- Rico: then use it.
--- EY : yea uh hello it is unfinished (i.e. I'm being stupid and can't make this work)
-noncomputable def Ptangle (a b c : Point) : (a≠b) → (b≠c) → real.dirangle :=
+-- show that if x y are on circle o r, then angle o x y and angle o y x are the same
+lemma circle_lemma_2 (X Y : ℂ) (O : ℂ) (r : ℝ) : X ≠ Y → O ≠ X → O ≠ Y → X ∈ circle O r → Y ∈ circle O r → angle O X Y = angle O Y X :=
 begin
-  intros hab hbc,
-  exact Angle (make_line a b hab) (make_line b c hbc),
+  intros XneY OneX OneY hX hY,
+  rw circle at hX,
+  rw circle at hY,
+  rw mem_sphere_iff_norm at hX,
+  rw mem_sphere_iff_norm at hY,
+  rw angle,
+  rw angle,
+  norm_num,
+  rw complex.arg_eq_arg_iff,
+  {
+    rw ← this_is_that1,
+    rw ← this_is_that1,
+    rw norm_div,
+    rw norm_div,
+    rw [← this_is_that2 Y O, dist_comm Y O, this_is_that2 O Y] at hY,
+    rw [← this_is_that2 X O, dist_comm X O, this_is_that2 O X] at hX,
+    rw [hX, hY],
+    -- simp takes so long tho!
+    sorry,
+  },
+  {
+    sorry,
+  },
+  {
+    sorry,
+  },
 end
 
+-- as a corollary, isosceles triangles have equal angles (remark - this does not prove the converse)
+lemma circle_lemma_3 (A B C : ℂ) : A ≠ B → A ≠ C → B ≠ C → dist A B = dist A C → angle A B C = angle A C B :=
+begin
+  intros AneB AneC BneC dAB_eq_dAC,
+  have h1 : B ∈ circle A (dist A B),
+  {
+    rw [circle, mem_sphere_iff_norm, dist_comm, this_is_that2],
+  },
+  have h2 : C ∈ circle A (dist A C),
+  {
+    rw [circle, mem_sphere_iff_norm, dist_comm, this_is_that2],
+  },
+  rw ← dAB_eq_dAC at h2,
+  exact circle_lemma_2 B C A (dist A B) BneC AneB AneC h1 h2,
+end
+
+-- TODO
+
+-- CIRCLES
+-- show that if x, y, z are on circle o r, then 2 * angle x y z = angle x o z
+-- (remark on above: this maybe annoying because idk if real.angle has_mul is true. if not, you can do angle x y z + angle x y z = angle x o z)
+-- show that if w x y z are on circle o r, then angle w x y + angle w z y = pi
+
+-- lemma circle_center_radius_decomp (circ ∈ Circles) (x : ℂ) :
+-- x ∈ circ →  ∃ (d : ℂ) , (complex.abs d = 1 ∧ x = d + ) :=
+-- begin
+
+-- end
+
+-- PERPENDICULAR BISECTOR
+-- define perpendicular as angle = pi/2 or 3pi/2
+-- define perpendicular bisector as the line through midpoint(a b), and midpoint(a b) + i * (a-b)
+-- show that the perpendicular bisector is perpendicular to the line a b
+-- show that p \in perpendicular bisector ↔ dist p a = dist p b
+
+
+-- TRIANGLE CONGRUENCE
+-- define triangle as an ORDERED triple of points
+-- remark: DO NOT axiom triangle a b c = triangle a c b, and axiom triangle a b c = triangle b a c, AFAIK this kills stuff due to permutations!
+-- define the equivalence relation (!! it needs to be an equivalence relation!!)  "direct congruence" of triangles as triangle a b c sim triangle d e f iff exists p : ℂ such that p a = d, p b = e, p c = f
+-- show that it is an equivalence relation!!
+-- show that angles are equal and lengths are equal for direct congruence
+-- prove asa, sas, and sss congruence for direct congruence
+-- define "flip congruence" as an equivalence relation (!!) such that a b c flipsim conjugate a b c
+-- now you can do above steps, using 
+-- define "congruence" as "direct congruent or flip congruent" and prove this is equivalence relation
+
+
+-- variables (a b c : ℂ) (r : ℝ)
+-- variable hab : a ≠ b
+-- variable circ : circle a r
+-- #check circ
